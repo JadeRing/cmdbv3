72d3565aafd58dffac5b437b1dc7bce75c7d072b
tianjintianyue <tianjintianyue@gmail.com>
2018-04-25 13:17:58 +0800

    add package app

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: jiashiwei
+@file: __init__.py.py
+@time: 2018/4/25 13:15
+"""
+
+$END$
---
+++ app/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: jiashiwei
+@file: test.py
+@time: 2018/4/25 13:15
+"""
+
